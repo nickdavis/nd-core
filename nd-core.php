d3fca2b645ba88b073ab39e96597be0c17cab011
Nick Davis <nick@iamnickdavis.com>
2017-03-06 16:11:02 +0100

    Bump

--- nd-core.php
+++ nd-core.php
@@ -3,7 +3,7 @@
  * Plugin Name: ND Core
  * Plugin URI: https://github.com/nickdavis/nd-core
  * Description: Core functionality plugin designed to work with your site specific core functionality plugin and your WordPress theme
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Nick Davis
  * Author URI: https://iamnickdavis.com
  *
